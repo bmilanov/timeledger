325404c95f6844872afb414845a541290a92c369
Biser Milanov <bmilanov11@gmail.com>
2020-07-18 14:44:17 +0300

    Add hours-per-day report

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let matches = App::from_yaml(yaml).get_matches();
     let app_options = options::Options::new(matches);
 
-    let t2 = timeledger::Timeledger::from_file(&app_options.ledger)?;
+    let timeledger = timeledger::Timeledger::from_file(&app_options.ledger)?;
 
-    println!("{:?}", t2);
+    println!("{}", timeledger.report_hours_per_day());
 
     Ok(())
 }
--- src/timeledger.rs
+++ src/timeledger.rs
@@ -13,6 +13,7 @@ struct Task {
 
 #[derive(Debug)]
 struct Day {
+    date: DateTime<Utc>,
     tasks: Vec<Task>
 }
 
@@ -30,6 +31,7 @@ impl Timeledger {
                 day.iter().map(
                     |(date, tasks)| {
                         Day {
+                            date: format!("{}T00:00:00Z", date).parse::<DateTime<Utc>>().unwrap(),
                             tasks: tasks.iter().map(
                                 |task| {
                                     Task {
@@ -55,5 +57,46 @@ impl Timeledger {
         let json = fs::read_to_string(filename)?;
 
         Timeledger::from_json(&json)
+    }
+
+    pub fn report_hours_per_day(self) -> String {
+        let mut report = String::new();
+
+        report.push_str("# ------------- #\n");
+        report.push_str("# Hours per Day #\n");
+        report.push_str("# ------------- #\n");
+
+        struct DayTotal {
+            date: DateTime<Utc>,
+            total_time: chrono::Duration
+        }
+
+        let days: Vec<DayTotal> = self.days.iter().map(
+            |day| {
+                let total_time = day.tasks.iter().map(
+                    |task| {
+                        task.end - task.start
+                    }
+                ).fold(chrono::Duration::zero(),
+                    |acc, x| {
+                        acc + x
+                    }
+                );
+
+                DayTotal {
+                    date: day.date,
+                    total_time: total_time
+                }
+            }
+        ).collect();
+
+        for day in days.iter() {
+            report.push_str(&format!("{}: {:.5}\n", day.date.format("%Y-%m-%d").to_string(),
+                                                    (day.total_time.num_minutes() as f64 / 60.0).to_string()));
+        }
+
+        report.push_str("# ------------- #\n");
+
+        report
     }
 }
