cf090704b66ed809ca6a35ba91cb9182f0bf2ee7
Biser Milanov <bmilanov11@gmail.com>
2020-07-19 19:06:45 +0300

    Remove comment

--- src/options.rs
+++ src/options.rs
@@ -5,7 +5,6 @@ pub struct Options {
 impl Options {
     pub fn new(cli_args: clap::ArgMatches) -> Options {
         Options {
-            // clap ensures Some(T)
             ledger: cli_args.value_of("ledger").unwrap().to_string()
         }
     }
